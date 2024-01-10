71fbf0701758ab8014318cc7c0a34d4be2852c35
Eddie Eby <eddie.eby@gmail.com>
2024-01-09 17:13:38 -0700

    strcutured directories and created products service

---
+++ products_service/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+'''
+responsible for keeping inventory
+reponsible for product information
+    price
+
+
+
+get prices
+get inventory
+get available products
+update inventory
+update prices?
+
+'''
