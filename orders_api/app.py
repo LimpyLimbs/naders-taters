e33375e22719cb4582bd86539f5ae6b46a56ed7f
eddie <eddie.eby@gmail.com>
2024-01-10 00:20:35 -0700

    basic orders service

---
+++ orders_api/app.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from uuid import uuid4, UUID
+from fastapi import FastAPI, HTTPException
+from starlette import status
+from schemas import CreateOrderSchema, GetOrderSchema
+
+app = FastAPI()
+
+ORDERS = []
+
+@app.get('/orders')
+def get_orders():
+    return ORDERS
+
+@app.post('/orders', status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
+def create_order(items: CreateOrderSchema):
+    order = items.model_dump()
+    order['order_id'] = uuid4()
+    order['created'] = datetime.utcnow()
+    ORDERS.append(order)
+    return order
+
+@app.get('/orders/{order_id}', response_model=GetOrderSchema)
+def get_order(order_id: UUID):
+    for order in ORDERS:
+        if order['order_id'] == order_id:
+            return order
+        raise HTTPException(status_code=404, detail=f'order {order_id} was not found')
+    
+@app.put('/orders/{order_id}', status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
+def update_order(order_id: UUID, updated_order: GetOrderSchema):
+    for index, order in enumerate(ORDERS):
+        if order['order_id'] == order_id:
+            ORDERS[index] = updated_order.model_dump()
+            return updated_order
+    raise HTTPException(status_code=404, detail=f'order {order_id} was not found')
+
+@app.delete('/orders/{order_id}', status_code=status.HTTP_202_ACCEPTED)
+def delete_order(order_id: UUID):
+    for index, order in enumerate(ORDERS):
+        if order['order_id'] == order_id['order_id']:
+            ORDERS.pop(index)
+            return f'Order {order_id} has been deleted'
+    raise HTTPException(status_code=404, detail=f'order {order_id} was not found')
+
+'''
+calculating the cost of the order will occur here
+the price of the order needs to be a key/value pair in the order anyway
+the payments service can just be a dumb endpoint which will only process payments
+    payments service will also update the payments service with updated inventory
+'''
--- products_service/app.py
+++ products_service/app.py
@@ -1,18 +1,160 @@
 from fastapi import FastAPI
+from schemas import UpdateInventorySchema
 
 app = FastAPI()
 
+PRODUCTS = {
+    'classic': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 364,
+            'medium': 362,
+            'large': 125
+        }
+    },
+    'barbeque': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 834,
+            'medium': 732,
+            'large': 15
+        }
+    },
+    'sour_cream_and_onion': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 1364,
+            'medium': 3462,
+            'large': 1472
+        }
+    },
+    'salt_and_vinegar': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 7864,
+            'medium': 9362,
+            'large': 645
+        }
+    },
+    'cheddar': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 5834,
+            'medium': 7325,
+            'large': 1455
+        }
+    },
+    'pizza': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 386,
+            'medium': 7322,
+            'large': 162
+        }
+    },
+    'jalapano': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 8264,
+            'medium': 3612,
+            'large': 1415
+        }
+    },
+    'kettle_cooked': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+        },
+        'inventory': {
+            'small': 7836,
+            'medium': 7762,
+            'large': 3125
+        }
+    },
+    'dill_pickle': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+            },
+        'inventory': {
+            'small': 2834,
+            'medium': 9736,
+            'large': 5125
+        }
+    },
+    'salt_and_pepper': {
+        'prices': {
+            'small': 1.99,
+            'medium': 2.99,
+            'large': 3.99
+        },
+        'inventory': {
+            'small': 3564,
+            'medium': 7562,
+            'large': 1455
+        }
+    }
+}
+
+@app.get('/products')
+def get_all():
+    return PRODUCTS
+
+@app.get('/products/prices')
+def get_prices():
+    prices = {}
+    for product_name, product_details in PRODUCTS.items():
+        prices[product_name] = product_details['prices']
+    return prices
+
+@app.get('/products/inventory')
+def get_inventory():
+    inventory = {}
+    for product_name, product_details in PRODUCTS.items():
+        inventory[product_name] = product_details['inventory']
+    return inventory
+
+# @app.put('products/inventory')
+# def update_inventory(inventory_details: UpdateInventorySchema):
+    '''
+    accepts a list of products with the ammount that is to be added or removed
+    must validate the flavor and the inventory amount
+    both the orders and the payments service will have to be able to update the inventory
+        payments when the order is paid for the inventory will need to be updated
+        orders will have to in the event that the order is canceled
+    '''
+
+
 '''
-responsible for keeping inventory
-reponsible for product information
-    price
-
-
-
-get prices
-get inventory
-get available products
-update inventory
-update prices?
-
+products service is responsible for maintaining a list of products and their details
 '''
